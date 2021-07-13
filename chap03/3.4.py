65be35898e98f185242f5d6b9e16465159ed2ae2
test <test@test.com>
2021-07-13 13:07:00 +0000

    ペアの全探索

---
+++ chap03/3.4.py
@@ -0,0 +1,14 @@
+from typing import Final
+
+SET1: set[int] = {1, 4, 5}
+SET2: set[int] = {3, 4, 9}
+INF: Final[int] = 999999999
+
+if __name__ == '__main__':
+    min = INF
+    for num1 in SET1:
+        for num2 in SET2:
+            if num1 + num2 > 10 and min > num1 + num2:
+                min = num1 + num2
+    
+    print("Result: {}".format(str(min)))
