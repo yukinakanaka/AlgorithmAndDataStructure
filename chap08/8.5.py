3c90dc30f688d3f1cb032dfe5be9253e63fbfec8
test <test@test.com>
2021-07-29 13:49:15 +0000

    双方向連結リスト bidirectional linked list

---
+++ chap08/8.5.py
@@ -0,0 +1,71 @@
+from typing import Generic, TypeVar
+
+T = TypeVar('T')
+
+
+class Node(Generic[T]):
+    value: str
+    next: T
+    prev: T
+
+    def __init__(self, v: str) -> None:
+        self.value = v
+        self.next = None
+        self.prev = None
+
+    def __repr__(self) -> str:
+        return self.value
+
+
+class BiLinkedList:
+    zero: Node
+
+    def __init__(self) -> None:
+        self.zero = Node('nil')
+
+
+    def insert(self, v: Node, p: Node=None) -> None:
+        if p is None:
+            p = self.zero
+
+        # p → x
+        # p → v → x
+        if p.next is not None:
+            p.next.prev = v
+            v.next = p.next
+
+        v.prev = p
+        p.next = v
+
+        print()
+
+    def print_list(self):
+        cur: Node = self.zero.next
+        while True:
+            print(cur.value)
+            if cur.next is None:
+                break
+            cur = cur.next
+
+    def delete(self, p: Node) -> None:
+        # 削除はO(1)
+        # x →　p → y
+        # x → y
+        p.next.prev = p.prev
+        p.prev.next = p.next
+
+
+if __name__ == '__main__':
+    one: Node = Node('one')
+    two: Node = Node('two')
+    three: Node = Node('three')
+
+    bll: BiLinkedList = BiLinkedList()
+    bll.insert(one)
+    bll.insert(two, one)
+    bll.insert(three, two)
+
+    bll.print_list()
+
+    bll.delete(two)
+    bll.print_list()
