cbefbf8df7d488d36e39546fbe608a13435e157b
test <test@test.com>
2021-07-29 13:27:02 +0000

    mv

--- chap08/8.2.py
+++ chap08/8.2.py
@@ -1,4 +1,15 @@
-from Node import Node
+from typing import Generic, Optional, TypeVar
+
+T = TypeVar('T')
+
+
+class Node(Generic[T]):
+    value: str
+    next: T
+
+    def __init__(self, value: str, next: Optional[T] = None) -> None:
+        self.value = value
+        self.next = next
 
 
 class LinkedList:
