dfe8613c6b2c79ae3a9e72f989d1694acd6314bf
test <test@test.com>
2021-08-05 14:13:31 +0000

    コンティニュー

---
+++ chap12/s_12.2.py
@@ -0,0 +1,21 @@
+import bisect
+
+if __name__ == '__main__':
+    a: list[int] = [5, 3, 4, 1, 2]
+    b: list[int] = [8, 5, 2, 3, 5]
+
+    M: int = 10
+
+    ra = sorted(a)
+    sum: int = 0
+    count: int = 0
+    for x in ra:
+        i: int = a.index(x)
+        for _ in range(b[i]):
+            sum += x
+            count += 1
+            if count == M:
+                break
+        else:
+            continue
+    print(f"{count}")
