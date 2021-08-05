7e34edf958b1d866f9bdad78d58c8dd584ee1f72
Yuki Nakamura <nakayuki@jp.ibm.com>
2021-08-05 00:47:02 +0000

    ヒープソート

--- chap12/12.2.py
+++ chap12/12.2.py
@@ -1,5 +1,8 @@
 # ヒープは検索には適していない
+from typing import Optional
 import math
+
+from typing import Optional
 
 
 class Heap:
@@ -25,8 +28,15 @@ class Heap:
     def top(self) -> int:
         return self.heap[0]
 
-    def pop(self) -> None:
+    def pop(self) -> Optional[int]:
+        if len(self.heap) == 0:
+            return None
+
+        top: int = self.heap[0]
         x: int = self.heap.pop()
+
+        if len(self.heap) == 0:
+            return x
 
         k: int = 0
         left: int = 2 * k + 1
@@ -46,6 +56,7 @@ class Heap:
             left = 2 * k + 1
             right = 2 * k + 2
         self.heap[k] = x
+        return top
 
     def print_heap(self) -> str:
         lf: int = 1
@@ -58,15 +69,31 @@ class Heap:
         print("-------------------------------")
 
 
-if __name__ == '__main__':
-    h: Heap = Heap()
-    h.push(5)
-    h.push(3)
-    h.push(7)
-    h.push(1)
-    h.push(10)
-    h.push(12)
-    h.push(8)
+def heap_sort(a: list[int]) -> list[int]:
+    heap: Heap = Heap()
+    for e in a:
+        heap.push(e)
 
-    h.pop()
-    h.print_heap()
+    result: list[int] = [-1] * len(a)
+    for i in range(len(a)):
+        result[len(a) - i - 1] = heap.pop()
+
+    return result
+
+
+if __name__ == '__main__':
+    # h: Heap = Heap()
+    # h.push(5)
+    # h.push(3)
+    # h.push(7)
+    # h.push(1)
+    # h.push(10)
+    # h.push(12)
+    # h.push(8)
+
+    # h.pop()
+    # h.print_heap()
+
+    a: list[int] = [4, 6, 7, 3, 9, 2, 0]
+
+    print(heap_sort(a))
